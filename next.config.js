49a5fe25aed5ce532651a22a0014bf94bd978b68
Firas Jerbi <34075959+fjerbi@users.noreply.github.com>
2022-02-03 15:09:51 +0100

    Fixing MongoDb Credentials

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   env: {
     DB_LOCAL_URI:
-      "mongodb+srv://admin:admin@cluster0.qhfve.mongodb.net/bookme?retryWrites=true&w=majority",
+      "your_mongo_db_uri,
   },
 };
